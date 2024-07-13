d9067a8ff36b268e8bba89a787140133e097623b
Artem Shapoval <it.fc1g@gmail.com>
2024-07-13 14:44:08 +0300

    added page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import './lib/i18n/i18n';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import NotFound from './pages/NotFound';
 
 export function App() {
   const { t } = useTranslation();
 
-  return <h1>{t('Hello world')}</h1>;
+  return (
+    <Routes>
+      <Route index element={<h1>{'Hello world'}</h1>} />
+      <Route path="*" element={<NotFound t={t} />} />
+    </Routes>
+  );
 }
 
 export function WrappedApp() {
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import T from '../types/T';
+
+function NotFound({ t }: T) {
+  return (
+    <section className="h-screen w-full bg-lightGrayish dark:bg-dark">
+      <div className="flex h-full items-center justify-center px-4 py-8 lg:px-6 lg:py-16">
+        <div className="text-center">
+          <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-primary-600 lg:text-9xl dark:text-primary-500">
+            404
+          </h1>
+          <p className="mb-4 text-3xl font-bold tracking-tight text-dark md:text-4xl dark:text-white">
+            {t('page.notFound.missing')}
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-600 dark:text-gray-300">
+            {t('page.notFound.info')}
+          </p>
+          <Link
+            to="/"
+            className="my-4 inline-flex rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+          >
+            {t('page.notFound.back')}
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NotFound;
