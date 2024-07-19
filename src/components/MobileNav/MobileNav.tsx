e786a4b3df04ae2c85f03aed08e0d3a19d652347
Artem Shapoval <it.fc1g@gmail.com>
2024-07-19 13:48:20 +0300

    added vercel.json

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -12,36 +12,36 @@ function MobileNav({ isOpen, t }: MobileNavProps) {
       className={`${isOpen ? 'absolute inset-x-0 top-0 z-[99999] mt-12 flex' : 'hidden'}`}
     >
       <ul className="nav mt-4 flex w-full flex-col rounded-lg border border-gray-100 bg-lightGrayish p-4 font-medium text-lightDark md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-lightDark dark:text-gray-300 [&>lin]:w-full">
-        <li className="group block rounded px-3 py-3 md:p-0">
+        <li className="group rounded px-3 py-3 md:p-0">
           <NavLink
-            className="px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
+            className="block px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
             to="/"
           >
             {t('page.navigation.home')}
           </NavLink>
         </li>
 
-        <li className="group block rounded px-3 py-3 md:p-0">
+        <li className="group rounded px-3 py-3 md:p-0">
           <NavLink
-            className="px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
+            className="block px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
             to="/about"
           >
             {t('page.navigation.about')}
           </NavLink>
         </li>
 
-        <li className="group block rounded px-3 py-3 md:p-0">
+        <li className="group rounded px-3 py-3 md:p-0">
           <NavLink
-            className="px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
+            className="block px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
             to="/vicinity?page=1"
           >
             {t('page.navigation.vicinity')}
           </NavLink>
         </li>
 
-        <li className="group block rounded px-3 py-3 md:p-0">
+        <li className="group rounded px-3 py-3 md:p-0">
           <NavLink
-            className="px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
+            className="block px-2 transition duration-300 group-hover:text-primary-800 group-hover:dark:text-primary-300"
             to="/cta"
           >
             {t('page.navigation.contacts')}
